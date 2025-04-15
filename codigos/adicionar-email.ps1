7e469dbbddaf73e350b24c8dcdd8d6529ef6091d
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2025-04-15 14:37:36 -0300

    arquivo para adicionar emails a lista

---
+++ codigos/adicionar-email.ps1
@@ -0,0 +1,10 @@
+$novoEmail        = $args[0]
+$dataHora         = Get-Date -Format "dd/MM/yyyy HH:mm:ss"
+$linha            = "$novoEmail,$dataHora"
+$caminhoDoArquivo = $caminhoArquivo = Join-Path -Path $PSScriptRoot -ChildPath "emails-bloqueados.txt"
+
+# Adiciona a nova linha ao arquivo
+Add-Content -Path $caminhoDoArquivo -Value $linha
+
+# Ordena e salva
+Get-Content -Path $caminhoDoArquivo | Sort-Object | Set-Content -Path $caminhoDoArquivo
