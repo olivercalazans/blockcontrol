30b457727eacdd97b6c13ff67a4bcbe4cf0d505a
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2025-04-15 14:39:49 -0300

    variável duplicada removida

--- codigos/adicionar-email.ps1
+++ codigos/adicionar-email.ps1
@@ -1,7 +1,7 @@
 $novoEmail        = $args[0]
 $dataHora         = Get-Date -Format "dd/MM/yyyy HH:mm:ss"
 $linha            = "$novoEmail,$dataHora"
-$caminhoDoArquivo = $caminhoArquivo = Join-Path -Path $PSScriptRoot -ChildPath "emails-bloqueados.txt"
+$caminhoDoArquivo = Join-Path -Path $PSScriptRoot -ChildPath "emails-bloqueados.txt"
 
 # Adiciona a nova linha ao arquivo
 Add-Content -Path $caminhoDoArquivo -Value $linha
