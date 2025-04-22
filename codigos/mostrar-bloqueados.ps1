737edfaee6b519a000183315edd3c8eda51c4dbd
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2025-04-22 13:47:56 -0300

    Changes for a better display

--- codigos/mostrar-bloqueados.ps1
+++ codigos/mostrar-bloqueados.ps1
@@ -3,10 +3,16 @@ $caminho       = "$PSScriptRoot\$nomeDoArquivo"
 $dados         = Get-Content -Path $caminho
 
 Write-Host "MOSTRANDO DADOS DE UM ARQUIVO EXTERNO"
+Write-Host ("-" * 71)
 
 foreach ($linha in $dados) {
     $partes     = $linha -split ","
-    $emailOuDom = $partes[0]
-    $data       = $partes[1]
-    Write-Host "$emailOuDom - $data"
- }
+    $emailOuDom = $partes[0].Trim()
+    $data       = $partes[1].Trim()
+    $len        = 69 - $emailOuDom.Length - $data.Length
+
+    if ($len -lt 1) { $len = 1 }
+
+    Write-Host ("| $emailOuDom" + (" " * $len) + "$data |")
+    Write-Host ("-" * 71)
+}
