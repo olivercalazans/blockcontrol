4f3b72c2bac0c3c4e97d317b5f0f03179b07669e
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2025-04-28 16:27:40 -0300

    bug fixed, command removed

--- codigos/ifrn-exchange-v3.ps1
+++ codigos/ifrn-exchange-v3.ps1
@@ -29,8 +29,8 @@ function Refresh-Box3 {
 $button1.Add_Click({
     $input1 = $textBoxInput1.Text
     if (![string]::IsNullOrWhiteSpace($input1)) {
-        Add-IPBlockListEntry -IPAddress $input1 | Out-Host -Wait -NoNewWindow
-        Start-Process powershell -ArgumentList "-NoProfile", "-ExecutionPolicy", "Bypass", "-File", "`"$PSScriptRoot\add.ps1`"", "ips", "`"$input1`"" -Wait -NoNewWindow
+        Add-IPBlockListEntry -IPAddress $input1 | Out-Host
+        Start-Process powershell -ArgumentList @("-NoProfile", "-ExecutionPolicy", "Bypass", "-File", "`"$PSScriptRoot\add.ps1`"", "ips", "`"$input1`"") -Wait -NoNewWindow
         Refresh-Box1
         [System.Windows.Forms.MessageBox]::Show("O IP `"$input1`" foi bloqueado")
     } elseif ([string]::IsNullOrWhiteSpace($input1)) {
@@ -44,8 +44,8 @@ $button1.Add_Click({
 $button2.Add_Click({
     $input2 = $textBoxInput2.Text
     if (![string]::IsNullOrWhiteSpace($input2)) {
-        Add-IPBlockListEntry -IPRange $input2| Out-Host -Wait -NoNewWindow
-        Start-Process powershell -ArgumentList "-NoProfile", "-ExecutionPolicy", "Bypass", "-File", "`"$PSScriptRoot\add.ps1`"", "ips", "`"$input2`"" -Wait -NoNewWindow
+        Add-IPBlockListEntry -IPRange $input2| Out-Host
+        Start-Process powershell -ArgumentList @("-NoProfile", "-ExecutionPolicy", "Bypass", "-File", "`"$PSScriptRoot\add.ps1`"", "ips", "`"$input2`"") -Wait -NoNewWindow
         Refresh-Box1
         [System.Windows.Forms.MessageBox]::Show("A faixa IP `"$input2`" foi bloqueada")
     } elseif ([string]::IsNullOrWhiteSpace($input2)) {
@@ -59,11 +59,10 @@ $button2.Add_Click({
 $button3.Add_Click({
     $input3 = $textBoxInput3.Text
     if (![string]::IsNullOrWhiteSpace($input3)) {
-        Set-SenderFilterConfig -BlockedDomainsAndSubdomains @{Add="$input3"} | Out-Host -Wait -NoNewWindow
-        Start-Process powershell -ArgumentList "-NoProfile", "-ExecutionPolicy", "Bypass", "-File", "`"$PSScriptRoot\add.ps1`"", "dominios", "`"$input3`"" -Wait -NoNewWindow
-        $blockedDomain = Get-SenderFilterConfig |  Select-Object -ExpandProperty BlockedSenders | Select-Object -ExpandProperty Address | Select-String "$input3" -Wait -NoNewWindow
+        Set-SenderFilterConfig -BlockedDomainsAndSubdomains @{Add="$input3"} | Out-Host
+        Start-Process powershell -ArgumentList @("-NoProfile", "-ExecutionPolicy", "Bypass", "-File", "`"$PSScriptRoot\add.ps1`"", "dominios", "`"$input3`"") -Wait -NoNewWindow
         Refresh-Box2
-        [System.Windows.Forms.MessageBox]::Show("O dominio `"$blockedDomain`" foi bloqueado")
+        [System.Windows.Forms.MessageBox]::Show("O dominio `"$input3`" foi bloqueado")
     } elseif ([string]::IsNullOrWhiteSpace($input3)) {
         [System.Windows.Forms.MessageBox]::Show("Input vazio ou em branco")
     } else {
@@ -76,11 +75,10 @@ $button3.Add_Click({
 $button4.Add_Click({
     $input4 = $textBoxInput4.Text
     if (![string]::IsNullOrWhiteSpace($input4)) {
-        Set-SenderFilterConfig -BlockedSenders @{Add="$input4"} -Wait -NoNewWindow
+        Set-SenderFilterConfig -BlockedSenders @{Add="$input4"}
         Start-Process powershell -ArgumentList @("-NoProfile", "-ExecutionPolicy", "Bypass", "-File", "`"$PSScriptRoot\add.ps1`"", "emails", "`"$input4`"") -Wait -NoNewWindow
-        $blockedEmail = Get-SenderFilterConfig |  Select-Object -ExpandProperty BlockedSenders | Select-Object -ExpandProperty Address | Select-String "$input4" -Wait -NoNewWindow
         Refresh-Box3
-        [System.Windows.Forms.MessageBox]::Show("O email `"$blockedEmail`" foi bloqueado")
+        [System.Windows.Forms.MessageBox]::Show("O email `"$input4`" foi bloqueado")
     } elseif ([string]::IsNullOrWhiteSpace($input4)) {
         [System.Windows.Forms.MessageBox]::Show("Input vazio ou em branco")
     } else {
@@ -96,7 +94,7 @@ $button4.Add_Click({
 $buttonRemove1.Add_Click({
     $inputRemove1 = $textBoxRemove1.Text
     if (![string]::IsNullOrWhiteSpace($inputRemove1)) {
-        Get-IPBlockListEntry | Where {$_.IPRange -eq $inputRemove1} | Remove-IPBlockListEntry -Confirm:$false | Out-Host -Wait -NoNewWindow
+        Get-IPBlockListEntry | Where {$_.IPRange -eq $inputRemove1} | Remove-IPBlockListEntry -Confirm:$false | Out-Host
         Start-Process powershell -ArgumentList @("-NoProfile", "-ExecutionPolicy", "Bypass", "-File", "`"$PSScriptRoot\remove.ps1`"", "ips", "`"$inputRemove1`"") -Wait -NoNewWindow
         Refresh-Box1
         [System.Windows.Forms.MessageBox]::Show("O IP $inputRemove1 foi removido.")
@@ -111,7 +109,7 @@ $buttonRemove1.Add_Click({
 $buttonRemove2.Add_Click({
     $inputRemove2 = $textBoxRemove2.Text
     if (![string]::IsNullOrWhiteSpace($inputRemove2)) {
-        Set-SenderFilterConfig -BlockedDomainsAndSubdomains @{Remove="$inputRemove2"} | Out-Host -Wait -NoNewWindow
+        Set-SenderFilterConfig -BlockedDomainsAndSubdomains @{Remove="$inputRemove2"} | Out-Host
         Start-Process powershell -ArgumentList @("-NoProfile", "-ExecutionPolicy", "Bypass", "-File", "`"$PSScriptRoot\remove.ps1`"", "dominios", "`"$inputRemove2`"") -Wait -NoNewWindow
         Refresh-Box2
         [System.Windows.Forms.MessageBox]::Show("O dominio $inputRemove2 foi removido.")
@@ -126,7 +124,7 @@ $buttonRemove2.Add_Click({
 $buttonRemove3.Add_Click({
     $inputRemove3 = $textBoxRemove3.Text
     if (![string]::IsNullOrWhiteSpace($inputRemove3)) {
-        Set-SenderFilterConfig -BlockedSenders @{Remove="$inputRemove3"} -Wait -NoNewWindow
+        Set-SenderFilterConfig -BlockedSenders @{Remove="$inputRemove3"}
         Start-Process powershell -ArgumentList @("-NoProfile", "-ExecutionPolicy", "Bypass", "-File", "`"$PSScriptRoot\remove.ps1`"", "emails", "`"$inputRemove3`"") -Wait -NoNewWindow
         Refresh-Box3
         [System.Windows.Forms.MessageBox]::Show("O email $inputRemove3 foi removido.")
