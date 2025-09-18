cacc37b16afd1df3f93ef0b2445fa3d3221e50ab
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2025-09-18 17:40:40 -0300

    Delete codigos directory

--- codigos/add.ps1
+++
@@ -1,9 +0,0 @@
-$nomeDoArquivo    = $args[0] + "-bloqueados.txt"
-$bloqueado        = $args[1]
-$dataHora         = Get-Date -Format "dd/MM/yyyy HH:mm:ss"
-$linha            = "$bloqueado - $dataHora"
-$caminhoDoArquivo = "$PSScriptRoot\$nomeDoArquivo"
-
-Add-Content -Path $caminhoDoArquivo -Value $linha
-
-Get-Content -Path $caminhoDoArquivo | Sort-Object | Set-Content -Path $caminhoDoArquivo
--- codigos/form.ps1
+++
@@ -1,5 +0,0 @@
-Add-Type -AssemblyName System.Windows.Forms
-
-$form = New-Object System.Windows.Forms.Form
-$form.Text = "Command Outputs and Input"
-$form.Size = New-Object System.Drawing.Size(1390,400)
